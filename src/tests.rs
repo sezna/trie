e95a13950c44228d1ec7a667d7e6163798d5b8f9
alex-hansen <ahansen@stackline.com>
2019-08-25 10:41:23 -0700

    set up tests and benching

---
+++ src/tests.rs
@@ -0,0 +1,82 @@
+
+extern crate rand;
+extern crate test;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+use rand::seq::SliceRandom;
+use test::Bencher;
+
+use super::trie::Trie;
+
+#[test]
+fn insert_and_search() {
+    let mut trie = Trie::new();
+    trie.add_word_mut("test");
+    trie.add_word_mut("alex");
+    trie.add_word_mut("mary");
+    assert!(trie.search("test"));
+    assert!(trie.search("alex"));
+    assert!(trie.search("mary"));
+}
+
+#[test]
+fn insert_and_predict() {
+    let mut trie = Trie::new();
+    trie.add_word_mut("test");
+    trie.add_word_mut("alex");
+    trie.add_word_mut("testosterone");
+    trie.add_word_mut("alexander");
+    assert!(trie.predict("alex").contains(&String::from("alexander")));
+    assert!(trie.predict("test").contains(&String::from("testosterone")));
+}
+
+#[bench]
+fn insert_a_lot(b: &mut Bencher) {
+    let mut trie = Trie::new();
+    b.iter(|| {
+        let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+        trie.add_word_mut(&rand_string);
+    });
+}
+
+#[bench]
+fn search_a_lot(b: &mut Bencher) {
+    let mut vec_of_strings:Vec<String> = Vec::new();
+    let mut trie = Trie::new();
+
+    for _ in 0..1000 {
+        let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+        vec_of_strings.push(rand_string.clone() );
+        trie.add_word_mut(&rand_string)
+    }
+
+    b.iter(|| {
+        trie.search(vec_of_strings.choose(&mut rand::thread_rng()).unwrap());
+    })
+
+}
+
+#[bench]
+fn predict_a_lot(b: &mut Bencher) {
+     let mut vec_of_strings:Vec<String> = Vec::new();
+    let mut trie = Trie::new();
+
+    for _ in 0..1000 {
+        let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+        vec_of_strings.push(rand_string.clone() );
+        trie.add_word_mut(&rand_string)
+    }
+
+    b.iter(|| {
+        trie.predict(vec_of_strings.choose(&mut rand::thread_rng()).unwrap());
+    })
+}
